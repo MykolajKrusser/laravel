400a5b6e4617ea7eda0a284f447b54f9823be420
Mykola <mkrusser@goldencomm.com>
2020-03-11 16:18:52 +0100

    works with auth, home page, header, styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle" src="https://picsum.photos/150/150" alt="picture">
+        </div>
+        <div class="col-9 pt-5">
+            <h1>Generator</h1>
+            <div class="d-flex">
+                <div class="mr-5"><strong>153</strong> posts</div>
+                <div class="mr-5"><strong>275</strong> articles</div>
+                <div class="mr-5"><strong>70</strong> stats</div>
             </div>
+            <p>
+                Lorem ipsum dolor sit.
+            </p>
         </div>
     </div>
 </div>
